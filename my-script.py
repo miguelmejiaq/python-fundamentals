6bcd99890ebc2602f241aca134d01cd525e2b1ce
miguelmejiaq <miguelmejiaq@outlook.com>
2021-05-19 20:32:26 -0500

    fullñy class 06

--- my-script.py
+++ my-script.py
@@ -9,17 +9,23 @@
 
 ### Solución
 # plantillas de texto.
-var_PreguntaNumero = 'Ingrese el {valor} numero: '
-var_preguntaNombre = 'Ingrese su nombre: '
-var_textoNumero1, var_textoNumero2 = 'primer','segundo'
-# Definimos las entradas
-var_numero1 = int(input(var_PreguntaNumero.format(valor = var_textoNumero1)))
-var_numero2 = int(input(var_PreguntaNumero.format(valor = var_textoNumero2)))
-var_nombre = input(var_preguntaNombre)
-# Salidas
-print("el promedio es: ",(var_numero1 + var_numero2) // 2)
-print("la suma de los 2 números es: ",var_numero1 + var_numero2)
-print("El cuadrado del primer número es: ",pow(var_numero1,2),"El cuadrado del segundo número es: ", var_numero2**2)
-print("Nombre en mayuscula: ",var_nombre.upper())
-print(var_nombre.lower().replace("i","*").title())
-print(var_nombre*3)
+# var_PreguntaNumero = 'Ingrese el {valor} numero: '
+# var_preguntaNombre = 'Ingrese su nombre: '
+# var_textoNumero1, var_textoNumero2 = 'primer','segundo'
+# # Definimos las entradas
+# var_numero1 = int(input(var_PreguntaNumero.format(valor = var_textoNumero1)))
+# var_numero2 = int(input(var_PreguntaNumero.format(valor = var_textoNumero2)))
+# var_nombre = input(var_preguntaNombre)
+# # Salidas
+# print("el promedio es: ",(var_numero1 + var_numero2) // 2)
+# print("la suma de los 2 números es: ",var_numero1 + var_numero2)
+# print("El cuadrado del primer número es: ",pow(var_numero1,2),"El cuadrado del segundo número es: ", var_numero2**2)
+# print("Nombre en mayuscula: ",var_nombre.upper())
+# print(var_nombre.lower().replace("i","*").title())
+# print(var_nombre*3)
+
+print("Comienzo")
+for i in range(3):
+    print(i, "Hola ", end="")
+    print()
+    print("FIN DE LA CORRIDA")
